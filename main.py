aa9c9bece09c5724eed92065749f0d3e95fe2e09
mcastre1 <miguelcastrejongal@gmail.com>
2024-03-20 10:23:52 -0600

    Able to create missing folders of given file extension.

--- main.py
+++ main.py
@@ -20,10 +20,28 @@ class DirectoryOrganizer():
         else:
             os.mkdir(self.dest_dir)
 
+        # Here we iterate through all files in the specified directory in init.
         for file in files:
+            # We split up the file name
             splitup_path = os.path.splitext(file)
-            file_name = splitup_path[0]
+            file_name = splitup_path[0]  # Save the file name on this variable.
+            # Save the file extension on this variable.
             file_extension = splitup_path[1]
+
+            # We check if the file we are working on has an extension.
+            if file_extension:
+                # We create a new path for our new folder here, by joining
+                # our destination path and the file extension we are currently
+                # working on without the dot.
+                new_path = os.path.join(
+                    self.dest_dir, file_extension.split('.')[1])
+
+                # If such new path doesnt exists then we create the directory
+                # For us to use in the future.
+                if not os.path.exists(new_path):
+                    os.mkdir(new_path)
+                else:  # If the path, folder already exists, then we move the current file there.
+                    pass
 
             print(f"File Name: {file_name}\n" +
                   f"File Extension: {file_extension}")
